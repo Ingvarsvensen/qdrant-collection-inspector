7aaeef37b53c8c837b8f22b9bba04b2ee2128e42
Ihor <oscara995577@outlook.com>
2024-12-18 11:23:43 +0100

    clean up

--- src/create_collection_n_test.py
+++ src/create_collection_n_test.py
@@ -1,38 +1,5 @@
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import VectorParams, Distance
-from src.config.settings import QDRANT_URL, QDRANT_API_KEY
-
-# def create_collection_with_shards(collection_name, shard_count):
-#     try:
-#         # Initialize Qdrant client
-#         client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
-#         print(f"Connected to Qdrant.")
-#
-#         # Check if the collection already exists
-#         if collection_name in [c.name for c in client.get_collections().collections]:
-#             print(f"Collection '{collection_name}' already exists. Skipping creation.")
-#             return
-#
-#         # Create collection with specified shard count
-#         client.create_collection(
-#             collection_name=collection_name,
-#             vectors_config=VectorParams(size=4, distance=Distance.COSINE),
-#             shard_number=shard_count
-#         )
-#         print(f"Collection '{collection_name}' created with {shard_count} shards.")
-#
-#     except Exception as e:
-#         print(f"Error: {e}")
-#
-#
-# if __name__ == "__main__":
-#     create_collection_with_shards("test_sharded_collection_2", 5)
-
-#############################################################################################################
-
-# --- Creating collection with parameters---
-from qdrant_client import QdrantClient
-from qdrant_client.http.models import VectorParams, Distance
 
 from src.config.settings import QDRANT_URL, QDRANT_API_KEY
 
--- src/problems_solving/fetch_unique_values.py
+++ src/problems_solving/fetch_unique_values.py
@@ -4,37 +4,6 @@ from qdrant_client import QdrantClient
 from src.config.settings import QDRANT_URL, QDRANT_API_KEY
 
 
-# def fetch_metadata():
-#     try:
-#         # Initialize Qdrant client
-#         client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
-#         print("Successfully connected to Qdrant.\n")
-#
-#         # Specify the collection
-#         collection_name = "test_sharded_collection_3"
-#
-#         # Scroll through the collection to fetch points and their payload
-#         response = client.scroll(
-#             collection_name=collection_name,
-#             limit=10,
-#             with_payload=True
-#         )
-#         points, _ = response
-#
-#         # Print points with payload
-#         print(f"Points in collection '{collection_name}':")
-#         for point in points:
-#             print(f"ID: {point.id}, Payload: {point.payload}")
-#
-#     except Exception as e:
-#         print(f"Error, something went wrong: {e}")
-#
-#
-# if __name__ == "__main__":
-#     fetch_metadata()
-
-#####################################################################################################
-
 # Fetch metadata values for a specific field from the first 100 points in the collection.
 def fetch_metadata_values(collection_name, field_name):
     client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
